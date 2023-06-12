3b8faf87ef924cf8a00a2c0bfbb3bed38d2ed773
Dailion <i482276@fhict.nl>
2023-06-12 19:25:36 +0200

    test

--- mac.py
+++ mac.py
@@ -5,13 +5,13 @@ import re
 # ifconfig eth0 up
 
 
-class MAC_changer: 
+class MAC_Changer: 
     def __init__(self):
         self.MAC = ""
     
     def get_MAC(self, iface):
         # Get the ifconfig output 
-        output = subprocess.run(["ifconfig", iface], shell=False, capture_output=True)
+        output = subprocess.run(["ipconfig", iface], shell=False, capture_output=True)
 
         # Print the output
         cmd_result = output.stdout.decode('utf-8')
@@ -35,17 +35,60 @@ class MAC_changer:
         print ("[+] Current MAC address is ", self.get_MAC(iface))
  
         # Shut down eth0 network interface
-        output = subprocess.run(["ifconfig", iface, "down"], shell= False, capture_output=True)
+        output = subprocess.run(["ipconfig", iface, "down"], shell= False, capture_output=True)
         print(output.stderr.decode('utf-8'))
 
         # Change the MAC address 
-        output = subprocess.run(["ifconfig", iface, "hw", "ether", new_mac], shell= False, capture_output=True)
+        output = subprocess.run(["ipconfig", iface, "hw", "ether", new_mac], shell= False, capture_output=True)
         print(output.stderr.decode('utf-8'))
 
         # Set start eth0 again
-        output = subprocess.run(["ifconfig", iface, "up", "ether", new_mac], shell= False, capture_output=True)
+        output = subprocess.run(["ipconfig", iface, "up", "ether", new_mac], shell= False, capture_output=True)
         print(output.stderr.decode('utf-8'))
 
         print("[+] Updated MAC address is ", self.get_MAC(iface))
         return self.get_MAC(iface)
 
+
+
+
+# class MAC_Changer:
+#     def __init__(self):
+#         self.current_mac = ""
+
+#     def get_current_mac(self, iface):
+#         output = subprocess.run(["ifconfig", iface], capture_output=True, text=True)
+#         cmd_result = output.stdout
+
+#         pattern = r'ether\s([\da-f]{2}(?::[\da-f]{2}){5})'
+#         regex = re.compile(pattern)
+#         match = regex.search(cmd_result)
+
+#         if match:
+#             self.current_mac = match.group(1)
+#         else:
+#             self.current_mac = None
+
+#         return self.current_mac
+
+#     def change_mac(self, iface, new_mac):
+#         if not self.current_mac:
+#             self.get_current_mac(iface)
+
+#         if self.current_mac:
+#             print(f"[+] Current MAC address: {self.current_mac}")
+
+#             subprocess.run(["ifconfig", iface, "down"])
+#             subprocess.run(["ifconfig", iface, "hw", "ether", new_mac])
+#             subprocess.run(["ifconfig", iface, "up"])
+
+#             new_mac = self.get_current_mac(iface)
+#             if new_mac == new_mac:
+#                 print(f"[+] Successfully changed MAC address to: {new_mac}")
+#                 return new_mac
+#             else:
+#                 print("[-] Failed to change MAC address.")
+#         else:
+#             print("[-] Unable to get the current MAC address.")
+
+#         return None
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from mac import MAC_changer
+from mac import MAC_Changer
 
 if __name__ == "__main__":
-    mc = MAC_changer()
+    mc = MAC_Changer()
     mac = mc.get_MAC("eth0")
     print(mac)
 
+    # The new MAC address that it will get. 
     curr_mac = mc.change_mac("eth0", "00:11:99:33:44:55")
     print(curr_mac)
+
+
+# if __name__ == "__main__":
+#     mc = MAC_Changer()
+#     mac = mc.get_current_mac("eth0")
+#     if mac:
+#         print(f"[+] Current MAC address: {mac}")
+
+#         # The new MAC address to set
+#         new_mac = "00:11:99:33:44:55"
+#         updated_mac = mc.change_mac("eth0", new_mac)
+#         if updated_mac:
+#             print(f"[+] Updated MAC address: {updated_mac}")
+#         else:
+#             print("[-] Failed to change MAC address.")
+#     else:
+#         print("[-] Unable to get the current MAC address.")
