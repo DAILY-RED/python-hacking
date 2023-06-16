d5f866505079c227aa4de48903f6084c4105efc1
Dailion <i482276@fhict.nl>
2023-06-16 19:36:18 +0200

    update

--- pac_man.py
+++ pac_man.py
@@ -1,5 +1,15 @@
 from scapy.all import IP, ICMP, sr1
 
-ip_layer = IP(scr="192.168.0.40", dst="www.google.com")
+# IP of my pc and the dest IP
+ip_layer = IP(scr="192.168.178.129", dst="www.google.com")
 
-icmp_req = ICMP
+icmp_req = ICMP()
+
+print(icmp_req.show)
+
+packet = ip_layer / icmp_req
+
+received_packet: sr1(packet)
+
+if received_packet:
+    print(received_packet.show())
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+packet = ip_layer / icmp_req
+
+# received_packet: sr1(packet)
+
+# if received_packet:
+#     print(received_packet.show())
