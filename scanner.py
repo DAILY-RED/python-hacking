13f53b611045b7ac2be237e005e0209253275863
Dailion <i482276@fhict.nl>
2023-06-17 12:22:32 +0200

    update

--- scanner.py
+++ scanner.py
@@ -5,7 +5,7 @@ import sys
 import time
 
 def arp_scan(iface, ip_range):
-    print("[+] Scannign ", ip_range)
+    print("[+] Scanning ", ip_range)
     curr_time = time.time()
     print ("[+] Scan started at ", time.ctime(curr_time))
     conf.verb = 0
@@ -13,7 +13,7 @@ def arp_scan(iface, ip_range):
     ether_layer = Ether(dst=broadcast)
     arp_layer = ARP(pdst=ip_range)
 
-    packet = ether_layer/arp_layer
+    packet = ether_layer / arp_layer
 
     ans, unans = srp(packet, iface=iface, timeout=2, inter=0.1)
 
@@ -21,12 +21,24 @@ def arp_scan(iface, ip_range):
         ip = rcv[ARP].psrc
         mac =rcv[Ether].src
         print(ip, mac)
-    duration = time.time() -curr_time
+
+    duration = time.time() - curr_time
     print("[+] Scan completed, Duration: ", duration)
 
 
-# scanner.py eth0 <IP>
 if __name__ == "__main__":
-    iface =sys.argv[1]
+    if len(sys.argv) != 3:
+        print("Usage: python scanner.py <interface> <IP range>")
+        sys.exit(1)
+
+    iface = sys.argv[1]
     ip_range = sys.argv[2]
     arp_scan(iface, ip_range)
+
+
+
+# # scanner.py eth0 <IP>
+# if __name__ == "__main__":
+#     iface =sys.argv[1]
+#     ip_range = sys.argv[2]
+#     arp_scan(iface, ip_range)
